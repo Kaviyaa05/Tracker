1044841d1d1ff6ed3ea91b73c2147241c0fb5c79
Yedunath03 <yedunathm0@gmail.com>
2024-02-12 11:17:47 +0530

    Backend for team memeber selection

---
+++ backend/TrackerAPI/TrackerAPI/Models/team.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrackerAPI
+{
+    public class team
+    {
+
+        [Key]
+
+        public int TeamID { get; set; }
+
+        [Required]
+        public int ProjectID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string TeamName { get; set; }
+
+        [StringLength(255)]
+        public string TeamMemberList { get; set; }
+    }
+}
