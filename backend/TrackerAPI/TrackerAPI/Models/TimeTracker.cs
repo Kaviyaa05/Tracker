bd05cee5602167132db1676c5cec3218c915fca1
Vishnupg06 <vishnupg1912@gmail.com>
2024-02-12 12:31:07 +0530

    Controller and Model class for TimeTracker commited without any conflicts.

---
+++ backend/TrackerAPI/TrackerAPI/Models/TimeTracker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrackerAPI.Models
+{
+    public class TimeTracker
+    {
+        public int UserID { get; set; }
+        public int ProjectID { get; set; }
+
+        [Key]
+        public int TaskID { get; set; }
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
+        public int TotalWorkingHours { get; set; }
+
+    }
+}
