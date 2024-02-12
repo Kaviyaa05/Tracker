d6ab96aba54467b31893bcfcf2517e1da478570d
kaviya <kaviyakavi55667@gmail.com>
2024-02-12 12:04:04 +0530

    LoginPage API

---
+++ backend/TrackerAPI/TrackerAPI/Controllers/DataController.cs
@@ -0,0 +1,24 @@
+﻿using TrackerAPI.DataAccessLayer;
+using System.Web.Http;
+using TrackerAPI.Utils;
+using System.Web.Http.Cors;
+
+namespace TrackerAPI.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class DataController : ApiController
+    {
+
+        // GET api/values
+        [Authorize]
+        public IHttpActionResult Get()
+        {
+            AuthDAO authDAO = new AuthDAO();
+         
+            var username = User.GetUsername();
+            var role = User.GetRole();
+
+            return Ok($"UserName: {username}, Role: {role}");
+        }
+    }
+}
---
+++ backend/TrackerAPI/TrackerAPI/Provider/JWTAuthorizationServerProvider.cs
@@ -0,0 +1,43 @@
+﻿using TrackerAPI.DataAccessLayer;
+using TrackerAPI.Models;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Diagnostics;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+{
+	AuthDAO _authDAO;
+	public SimpleAuthorizationServerProvider()
+	{
+		this._authDAO = new AuthDAO();
+	}
+
+
+	public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+	{
+		context.Validated();
+	}
+
+	public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+	{
+		User user = _authDAO.GetUserByUsername(context.UserName);
+		if (user != null && user.Password.Trim().Equals(context.Password)) 
+		{
+			var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+			identity.AddClaim(new Claim("username", context.UserName));
+			identity.AddClaim(new Claim("role", user.Role));
+			context.Validated(identity);
+		}
+		else
+		{
+			context.SetError("invalid_grant", "The user name or password is incorrect.");
+			return;
+		}
+
+
+	}
+
+
+}
