d6024eb9083f64a8d0254a0084f59a359d3648d2
UG-Shruti <ugshruti21@gmail.com>
2024-02-12 11:03:45 +0530

    Initial push for the report backend

---
+++ backend/TrackerAPI/TrackerAPI/Models/Details.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrackerAPI.Models
+{
+    public class Details
+    {
+        public int ProjectID { get; set; }
+        public string ProjectName { get; set; }
+        public int TaskID { get; set; }
+        public int UserID { get; set; }
+        public string ProjectPriority { get; set; }
+        public string TaskName { get; set; }
+        public string TaskPriority { get; set; }
+        
+        public string TaskType { get; set; }
+        public string Description { get; set; }
+        public string Assigned_By { get; set; }       
+        public string Teams { get; set;}        
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ backend/TrackerAPI/TrackerAPI/Models/Project.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrackerAPI.Models
+{
+    public class Project
+    {
+        internal int projectId;
+
+        public int ProjectID { get; set; }
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Priority { get; set; }
+        public string Team { get; set; }
+        public string Owner { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}
