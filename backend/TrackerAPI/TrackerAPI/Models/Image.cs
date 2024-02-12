173063d0a03a5b35c479b5343311f51846aae433
dhanabal m <dhanz7112002@gmail.com>
2024-02-12 11:09:31 +0530

    Controller and Model class backend code is updated for imageuploader.

---
+++ backend/TrackerAPI/TrackerAPI/Models/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrackerAPI.Models
+{
+    public class Image
+    {
+        //Implementing getter setter
+        public int Id { get; set; }
+
+        public byte[] PhotoData { get; set; }
+    }
+}
