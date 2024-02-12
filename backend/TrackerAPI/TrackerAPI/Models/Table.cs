746ca7f2cee4be169e1e49455ee355afb006055c
Lakshmipriya-18 <lp476340@gmail.com>
2024-02-12 10:25:10 +0530

    Backend for Audit

---
+++ backend/TrackerAPI/TrackerAPI/Models/Table.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrackerAPI.Models
+{
+    public class Table
+    {
+        public static string connect = "Data Source=DESKTOP-8BO2DOD\\SQLEXPRESS;Initial Catalog=Audit;Integrated Security=True";
+    }
+}
