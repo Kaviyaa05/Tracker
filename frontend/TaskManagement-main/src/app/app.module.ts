dcaf19c13459db1e230011a365db1e3bfb1f40b9
Kaviyaa05 <122660421+Kaviyaa05@users.noreply.github.com>
2024-02-13 10:37:13 +0530

    Update app.module.ts

--- frontend/TaskManagement-main/src/app/app.module.ts
+++ frontend/TaskManagement-main/src/app/app.module.ts
@@ -40,7 +40,7 @@ import { ProjectburndownComponent } from './report/projectburndown/projectburndo
 import { ShowTskcomponent } from './task/form/show-tsk/show-tsk.component';
 import { AddEditComponent } from './task/form/add-edit/add-edit.component';
 import { SharedService } from './task/form/shared.service';
-import { HttpClientModule} from '@angular/common/http';
+
 
 
 
@@ -76,7 +76,7 @@ import { HttpClientModule} from '@angular/common/http';
     FontAwesomeModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+   
   ],
   providers: [
     provideClientHydration(),SharedService
