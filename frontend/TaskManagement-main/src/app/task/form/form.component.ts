8461eac473e39700942cef2d8b89270b92ce7e17
KAVIN0802 <kavinkishore0802@gmail.com>
2024-02-13 17:12:09 +0530

    modified dropdown in add taskform and rectified eoor in delete operation and modified delete operation

--- frontend/TaskManagement-main/src/app/task/form/form.component.ts
+++ frontend/TaskManagement-main/src/app/task/form/form.component.ts
@@ -1,56 +1,57 @@
-import { Component,Input,OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { SharedService } from './shared.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css'] // Correct the typo 'styleUrl' to 'styleUrls'
+  styleUrls: ['./form.component.css']
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
   
-  constructor(private service:SharedService) {}
-  @Input() tsk:any=[];
-  TaskId:string="";
-  UserId:string="";
-  Taskname:string="";
-  TaskType:string="";
-  Priority:string="";
-  CreatedBy:string="";
-  StartDate:string="";
-  EndDate:string="";
-  Status:string="";
-  Description:string="";
+  constructor(private service: SharedService) {}
 
-    
-    ngOnInit(): void {
-      this.TaskId=this.tsk.TaskId;
-      this.UserId=this.tsk.UserId;
-      this.Taskname=this.tsk.Taskname;
-      this.TaskType=this.tsk.TaskType;
-      this.Priority=this.tsk.Priority;
-      this.CreatedBy=this.tsk.CreatedBy;
-      this.StartDate=this.tsk.StartDate;
-      this.EndDate=this.tsk.EndDate;
-      this.Status=this.tsk.Status;
-      this.Description=this.tsk.Description;
-    }
+  @Input() tsk: any = [];
+  TaskId: string = "";
+  UserId: string = "";
+  Taskname: string = "";
+  TaskType: string = "";
+  Priority: string = "";
+  CreatedBy: string = "";
+  StartDate: string = "";
+  EndDate: string = "";
+  Status: string = "";
+  Description: string = "";
 
-    addTask(){
-      var val = {
-        TaskId:this.TaskId,
-        UserId:this.UserId,
-        Taskname:this.Taskname,
-        TaskType:this.TaskType,
-        Priority:this.Priority,
-        CreatedBy:this.CreatedBy,
-        StartDate:this.StartDate,
-        EndDate:this.EndDate,
-        Status:this.Status,
-        Description:this.Description};
+  ngOnInit(): void {
+    this.TaskId = this.tsk.TaskId;
+    this.UserId = this.tsk.UserId;
+    this.Taskname = this.tsk.Taskname;
+    this.TaskType = this.tsk.TaskType;
+    this.Priority = this.tsk.Priority;
+    this.CreatedBy = this.tsk.CreatedBy;
+    this.StartDate = this.tsk.StartDate;
+    this.EndDate = this.tsk.EndDate;
+    this.Status = this.tsk.Status;
+    this.Description = this.tsk.Description;
+  }
+
+  addTask() {
+    var val = {
+      TaskId:this.TaskId,
+      UserId:this.UserId,
+      Taskname:this.Taskname,
+      TaskType:this.TaskType,
+      Priority:this.Priority,
+      CreatedBy:this.CreatedBy,
+      StartDate:this.StartDate,
+      EndDate:this.EndDate,
+      Status:this.Status,
+      Description:this.Description};
       this.service.addTask(val).subscribe(res=>{
         alert(res.toString());
       });
-    }
+
+  }
     updateTask(){
       var val = {
         TaskId:this.TaskId,
