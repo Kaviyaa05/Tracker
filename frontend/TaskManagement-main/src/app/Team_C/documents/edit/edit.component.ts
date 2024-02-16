fd8b088447eba7cc447bb999f77937ae7a1895de
Kaniga16 <kaniga1610@gmail.com>
2024-02-16 12:28:10 +0530

    document frontend

---
+++ frontend/TaskManagement-main/src/app/Team_C/documents/edit/edit.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DocumentService } from '../service/document.service';
+
+interface Note {
+  Title: string;
+  Content: string;
+}
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.css']
+})
+export class EditComponent implements OnInit {
+  noteId!: number;
+  note: Note = { Title: '', Content: '' };
+
+  constructor(private route: ActivatedRoute, private router: Router, private documentService: DocumentService) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.noteId = +params['id'];
+      this.getData();
+    });
+  }
+
+  getData() {
+    this.documentService.getNoteById(this.noteId).subscribe((res: Note) => {
+      this.note = res;
+    });
+  }
+
+  returnNote() {
+    this.documentService.updateNote(this.noteId, this.note).subscribe(
+      (data) => {
+        console.log('Note updated successfully:', data);
+        this.router.navigate(['/documents']); 
+      },
+    )
+  }
+}
