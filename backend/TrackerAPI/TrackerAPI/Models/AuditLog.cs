b0c2f8e6ba957afdc8498c98be35c8d1563ed5e7
Kaviyaa05 <122660421+Kaviyaa05@users.noreply.github.com>
2024-02-16 15:47:41 +0530

    Update AuditLog.cs

--- backend/TrackerAPI/TrackerAPI/Models/AuditLog.cs
+++ backend/TrackerAPI/TrackerAPI/Models/AuditLog.cs
@@ -8,7 +8,7 @@ namespace TrackerAPI.Models
     public class AuditLog
     {
         public DateTime Date_And_Time { get; set; }
-        public string userName { get; set; }
+        public string UserName { get; set; }
         public string Module { get; set; }
         public string Action { get; set; }
     }
